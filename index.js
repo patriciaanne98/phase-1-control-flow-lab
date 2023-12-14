1cd7a37d2c71e3e1bb158819af89fefd845b0e65
Patricia Frogoso <pfrogoso@gmail.com>
2023-12-13 21:19:56 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+    if (feet <= 400) {
+      const scuberGreetingForFeet = 'This one is on me!';
+      return scuberGreetingForFeet;
+    } else if (feet > 400 && feet <= 2000) {
+      const scuberGreetingForFeet = 'That will be twenty bucks.';
+      return scuberGreetingForFeet;
+    } else if (feet > 2000 && feet <= 2500) {
+      const scuberGreetingForFeet = 'I will gladly take your thirty bucks.';
+      return scuberGreetingForFeet; 
+    } else {
+      const scuberGreetingForFeet = 'No can do.';
+      return scuberGreetingForFeet;
+    }
+  }
+function ternaryCheckCity(city){
+  if (city === 'NYC') {
+    const ternaryCheckCity = 'Ok, sounds good.';
+    return ternaryCheckCity;
+  } else {
+    return 'No go.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if (tip === 'generous') {
+    let switchOnCharmFromTip = 'Thank you so much.';
+    return switchOnCharmFromTip;
+  } else if (tip === 'not as generous') {
+    return 'Thank you.';
+  } else {
+    let switchOnCharmFromTip = 'Bye.';
+    return 'Bye.';
+  }
+  }
